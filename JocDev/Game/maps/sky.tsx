732d1488b5446c7bd3779e83043e890a42e720c4
didaclis <36189205+didaclis@users.noreply.github.com>
2018-10-07 23:21:59 +0200

    map1

---
+++ JocDev/Game/maps/sky.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="sky" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="725" columns="29">
+ <image source="Background.png" width="960" height="832"/>
+ <terraintypes>
+  <terrain name="sky" tile="59"/>
+ </terraintypes>
+ <tile id="59" terrain="0,0,0,0"/>
+</tileset>
